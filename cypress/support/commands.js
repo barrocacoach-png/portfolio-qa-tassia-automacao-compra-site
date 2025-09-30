4616db7ad7d6e65eac7b84e04df401a84f92dd10
Tássia Barroca <barrocacoach@gmail.com>
2025-09-29 22:43:01 -0300

    chore: adiciona comandos customizados ao suporte do Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Custom command for login
+Cypress.Commands.add('login', () => {
+	cy.visit('https://www.saucedemo.com/');
+	cy.get('[data-test="login-container"]').should('be.visible');
+	cy.get('[data-test="username"]').type('standard_user');
+	cy.get('[data-test="password"]').type('secret_sauce');
+	cy.get('[data-test="login-button"]').click();
+	cy.url().should('include', '/inventory.html');
+});
+
+// Custom command to add items to cart
+Cypress.Commands.add('addItemsToCart', (count = 1) => {
+	cy.get('.inventory_item').each(($item, index) => {
+		if (index < count) {
+			cy.wrap($item).contains('Add to cart').click();
+		}
+	});
+});
+
+// Custom command to proceed to checkout with personal data
+Cypress.Commands.add('checkoutWithPersonalData', (firstName, lastName, postalCode) => {
+  cy.get('[data-test="checkout"]').should('be.visible').click();
+  cy.url().should('include', '/checkout-step-one.html');
+  cy.get('[data-test="firstName"]').type(firstName);
+  cy.get('[data-test="lastName"]').type(lastName);
+  cy.get('[data-test="postalCode"]').type(postalCode);
+  cy.get('[data-test="continue"]').click();
+  cy.url().should('include', '/checkout-step-two.html');
+});
