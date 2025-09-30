54fb4c95125e516b732060c7609cc84e8fc69341
Tássia Barroca <barrocacoach@gmail.com>
2025-09-29 22:53:01 -0300

    refactor(login): Padroniza estrutura de casos de teste

--- cypress/e2e/login-do-cliente/login-do-cliente.cy.js
+++ cypress/e2e/login-do-cliente/login-do-cliente.cy.js
@@ -5,50 +5,58 @@ describe('Página de login', () => {
     cy.get('[data-test="login-container"]').should('be.visible');
   })
 
+  // ------------------------------------------------------
   //Login com usuário e senha válidos
+  // ------------------------------------------------------
 
   it('Deve preencher os campos de usuário e senha corretamente e autenticar o usuário no site', () => {
-  
+
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
 
     // Asserção: verificar se o usuário foi redirecionado para a página de produtos
-  cy.url().should('include', '/inventory.html');
+    cy.url().should('include', '/inventory.html');
   })
 
-//Login sem usuário e com senha válida
+  // ------------------------------------------------------
+  //Login sem usuário e com senha válida
+  // ------------------------------------------------------
 
-it('Deve preencher o campo de senha corretamente e deixar o de usuário vazio; o usuário não deve ser autenticado no site', () => {
-  
+  it('Deve preencher o campo de senha corretamente e deixar o de usuário vazio; o usuário não deve ser autenticado no site', () => {
+
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
 
     // Asserção: verificar se a mensagem de erro é exibida
-  cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
+    cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
   })
 
-//Login com usuário e sem senha
+  // ------------------------------------------------------
+  //Login com usuário e sem senha
+  // ------------------------------------------------------
 
-it('Deve preencher o campo de usuário corretamente e deixar o de senha vazio; o usuário não deve ser autenticado no site', () => {
-  
+  it('Deve preencher o campo de usuário corretamente e deixar o de senha vazio; o usuário não deve ser autenticado no site', () => {
+
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="login-button"]').click();
 
     // Asserção: verificar se a mensagem de erro é exibida
-  cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required');
+    cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required');
   })
 
-//Login com usuário bloqueado
+  // ------------------------------------------------------
+  //Login com usuário bloqueado
+  // ------------------------------------------------------
 
-it('Deve preencher o campo de usuário com usuário bloqueado e senha válida; o usuário não deve ser autenticado no site', () => {
-  
+  it('Deve preencher o campo de usuário com usuário bloqueado e senha válida; o usuário não deve ser autenticado no site', () => {
+
     cy.get('[data-test="username"]').type('locked_out_user');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
 
     // Asserção: verificar se a mensagem de erro é exibida
-  cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.');
+    cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.');
   })
 
-  })
+})
