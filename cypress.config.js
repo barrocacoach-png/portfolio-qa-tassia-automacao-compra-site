8b1a7dff83865e6520fa8e802415ed8a835d3a53
Tássia Barroca <barrocacoach@gmail.com>
2025-10-05 01:41:38 -0300

    chore: Adiciona configurações para merge de relatórios e ambiente de CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "cpvbat",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     pageLoadTimeout: 120000, // Aumenta o tempo para 120 segundos
+    video: true,
+    reporter:'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
+    baseUrl: 'https://www.saucedemo.com',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
   },
 });
